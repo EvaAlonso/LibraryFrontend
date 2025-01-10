31078eff97e11803fef86ca1b8df7de3b81e3759
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-10 09:05:00 +0100

    Added Admin page to Router

---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/molecules/Header/Header"
+
+const Admin = () => {
+    return (
+      <>
+        <Header />
+        <p>ADMIN PAGE</p>
+      </>
+    )
+  }
+  
+  export default Admin
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,15 @@
-
-// REACT-ROUTER-DOM Components
 import { createBrowserRouter } from "react-router-dom";
 
-// Pages
 import Home from "../pages/Home/Home.jsx";
+import Admin from "../pages/Admin/Admin.jsx";
 
-/* ROUTER */
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path: "/admin",
+        element: <Admin />
     }
 ])

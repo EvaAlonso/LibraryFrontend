9a4b046009c3cdfbf48daadd77a8e867f84d4f21
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-10 08:49:36 +0100

    Added ADMIN + More info Buttons

---
+++ src/components/atoms/AdminButton/AdminButton.jsx
@@ -0,0 +1,11 @@
+import "./admin-button.css";
+
+const AdminButton = () => {
+  return (
+    <button className="admin-btn-container">
+      <p className="admin-btn-text">ADMIN</p> 
+    </button>
+  )
+}
+
+export default AdminButton
---
+++ src/components/atoms/ShowButton/ShowButton.jsx
@@ -0,0 +1,12 @@
+import "./show-button.css";
+
+const ShowButton = () => {
+  return (
+    <button className="show-btn-container">
+      {/* Add Card Link */}
+      <p className="show-btn-text">Show more</p> 
+    </button>
+  )
+}
+
+export default ShowButton

4c3857021f6c93f805f338bb5805b6f0ec838f69
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-10 11:13:23 +0100

    Changed Form Structure in router

--- src/components/molecules/Form/Form.jsx
+++ src/components/molecules/Form/Form.jsx
@@ -1,9 +1,17 @@
+import FileInput from "../../atoms/FileInput/FileInput.jsx";
+import TextInput from "../../atoms/TextInput/TextInput.jsx";
+
 import "./form.css";
 
 const Form = () => {
   return (
     <form>
-        
+        <TextInput />
+        <TextInput />
+        <TextInput />
+        <TextInput />
+        <FileInput />
+        <TextInput />
     </form>
   )
 }

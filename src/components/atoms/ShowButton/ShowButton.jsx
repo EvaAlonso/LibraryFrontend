e5a80126123b0895defee707cd9e850f0ea01df1
nawud <duwan17@gmail.com>
2025-01-11 22:20:57 +0100

    estilos css añadidos

--- src/components/atoms/ShowButton/ShowButton.jsx
+++ src/components/atoms/ShowButton/ShowButton.jsx
@@ -1,8 +1,9 @@
 import "./show-button.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const ShowButton = () => {
   return (
-    <button className="show-btn-container">
+    <button className="show-btn-container mx-auto ">
       {/* Add Card Link */}
       <p className="show-btn-text">Show more</p> 
     </button>
